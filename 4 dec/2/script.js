4ebba456101a6adbb9c259f89929e2e07928b6f0
Suhel <Suhelmulla24@gmail.com>
2022-12-04 23:58:25 +0530

    bg colour changing solved

--- 4 dec/2/script.js
+++ 4 dec/2/script.js
@@ -1,22 +1,13 @@
-const increase_button = document.getElementsByClassName("increase_btn");
-const counting = document.getElementById("counter_dispaly");
-var count = 0;
-// increase_button.addEventListener("click", function () {
-//   count++;
-//   counting.innerHTML = " " + count;
-// });
-function increaseCounting() {
-  count++;
-  printCount();
-}
-function decreaseCounting() {
-  count--;
-  printCount();
-}
-function reasetCounting() {
-  count = 0;
-  printCount();
-}
-function printCount() {
-  counting.innerHTML = count;
-}
+document.getElementById("gray-circle").addEventListener("click", function () {
+  document.body.style.backgroundColor = "gray";
+  document.getElementById("gray-circle").setAttribute("class", "selected");
+});
+document.getElementById("white-circle").addEventListener("click", function () {
+  document.body.style.backgroundColor = "white";
+});
+document.getElementById("blue-circle").addEventListener("click", function () {
+  document.body.style.backgroundColor = "blue";
+});
+document.getElementById("yellow-circle").addEventListener("click", function () {
+  document.body.style.backgroundColor = "yellow";
+});
